5170e34a0a0a009e8ab2c9f99837ef2d137b8ab8
Dostonbek <57319534+doston12@users.noreply.github.com>
2019-12-10 15:37:45 +0500

    Update StudentTLessonRepository.java

--- src/main/src/com/teacher/uz/my/repositories/StudentTLessonRepository.java
+++ src/main/src/com/teacher/uz/my/repositories/StudentTLessonRepository.java
@@ -3,8 +3,5 @@ package com.teacher.uz.my.repositories;
 import com.teacher.uz.my.domains.StudentTLesson;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-/**
- * Created by Shoh Jahon on 19.05.2018.
- */
 public interface StudentTLessonRepository extends JpaRepository<StudentTLesson,Long> {
 }
