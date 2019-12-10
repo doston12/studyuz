c740faebd5601fa34b07e937410fb61c8690b83a
Dostonbek <57319534+doston12@users.noreply.github.com>
2019-12-10 15:37:14 +0500

    Update ThumbnailRepository.java

--- src/main/src/com/teacher/uz/my/repositories/ThumbnailRepository.java
+++ src/main/src/com/teacher/uz/my/repositories/ThumbnailRepository.java
@@ -5,9 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-/**
- * Created by Shoh Jahon on 04.05.2018.
- */
 public interface ThumbnailRepository extends JpaRepository<Thumbnail,Long> {
     public List<Thumbnail> findThumbnailByArticleId(Long id);
 }
