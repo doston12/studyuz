ab1f2d5c451f581ba34b46dfa16d76dc78d4ccc0
Dostonbek <57319534+doston12@users.noreply.github.com>
2019-12-10 15:37:30 +0500

    Update TRoomLessonRepository.java

--- src/main/src/com/teacher/uz/my/repositories/TRoomLessonRepository.java
+++ src/main/src/com/teacher/uz/my/repositories/TRoomLessonRepository.java
@@ -5,9 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-/**
- * Created by Shoh Jahon on 18.05.2018.
- */
 public interface TRoomLessonRepository extends JpaRepository<TRoomLesson,Long> {
     public List<TRoomLesson> findTRoomLessonsByUserId(Long id);
 }
