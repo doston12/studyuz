354ebdf77a4172c33fac3b5ff804b7a8e177fc1c
Dostonbek <57319534+doston12@users.noreply.github.com>
2019-12-10 15:40:02 +0500

    Update LogTestService.java

--- src/main/src/com/teacher/uz/my/services/LogTestService.java
+++ src/main/src/com/teacher/uz/my/services/LogTestService.java
@@ -6,9 +6,6 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-/**
- * Created by Shoh Jahon on 04.05.2018.
- */
 @Service
 public class LogTestService {
     private LogTestRepository logTestRepository;
