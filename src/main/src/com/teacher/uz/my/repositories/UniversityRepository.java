02cf3091ef3288ef1a229a0622724a3a2212220d
Dostonbek <57319534+doston12@users.noreply.github.com>
2019-12-10 15:36:44 +0500

    Update UniversityRepository.java

--- src/main/src/com/teacher/uz/my/repositories/UniversityRepository.java
+++ src/main/src/com/teacher/uz/my/repositories/UniversityRepository.java
@@ -3,8 +3,5 @@ package com.teacher.uz.my.repositories;
 import com.teacher.uz.my.domains.University;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-/**
- * Created by Shoh Jahon on 12.04.2018.
- */
 public interface UniversityRepository extends JpaRepository<University,Long> {
 }
