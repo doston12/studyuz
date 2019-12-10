74783f7789b8e5821e672a9484a8625068d4c515
Dostonbek <57319534+doston12@users.noreply.github.com>
2019-12-10 15:39:03 +0500

    Update ArticleRepository.java

--- src/main/src/com/teacher/uz/my/repositories/ArticleRepository.java
+++ src/main/src/com/teacher/uz/my/repositories/ArticleRepository.java
@@ -5,9 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-/**
- * Created by Shoh Jahon on 04.05.2018.
- */
 public interface ArticleRepository extends JpaRepository<Article,Long> {
     public List<Article> findArticleBySubjectId(Long id);
 }
