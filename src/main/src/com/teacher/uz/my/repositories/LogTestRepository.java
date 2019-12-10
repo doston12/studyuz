4f69aebb4ea6530965e3de00e16fab7cb024b064
Dostonbek <57319534+doston12@users.noreply.github.com>
2019-12-10 15:38:13 +0500

    Update LogTestRepository.java

--- src/main/src/com/teacher/uz/my/repositories/LogTestRepository.java
+++ src/main/src/com/teacher/uz/my/repositories/LogTestRepository.java
@@ -3,8 +3,5 @@ package com.teacher.uz.my.repositories;
 import com.teacher.uz.my.domains.LogTest;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-/**
- * Created by Shoh Jahon on 04.05.2018.
- */
 public interface LogTestRepository extends JpaRepository<LogTest,Long> {
 }
