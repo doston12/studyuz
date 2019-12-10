948f7e68dbe611df4e1d02d5c11984d7de9a859f
Dostonbek <57319534+doston12@users.noreply.github.com>
2019-12-10 15:38:07 +0500

    Update RepassLogRepository.java

--- src/main/src/com/teacher/uz/my/repositories/RepassLogRepository.java
+++ src/main/src/com/teacher/uz/my/repositories/RepassLogRepository.java
@@ -3,8 +3,5 @@ package com.teacher.uz.my.repositories;
 import com.teacher.uz.my.domains.RepassLog;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-/**
- * Created by Shoh Jahon on 04.05.2018.
- */
 public interface RepassLogRepository extends JpaRepository<RepassLog,Long> {
 }
