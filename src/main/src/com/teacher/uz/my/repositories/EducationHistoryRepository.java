e00fc9ad7dbde12545eaf5a10d594f627b7d5c0f
Dostonbek <57319534+doston12@users.noreply.github.com>
2019-12-10 15:38:42 +0500

    Update EducationHistoryRepository.java

--- src/main/src/com/teacher/uz/my/repositories/EducationHistoryRepository.java
+++ src/main/src/com/teacher/uz/my/repositories/EducationHistoryRepository.java
@@ -5,9 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
-/**
- * Created by Shoh Jahon on 04.05.2018.
- */
 @Repository
 @Transactional
 public interface EducationHistoryRepository extends JpaRepository<EducationHistory,Long> {
